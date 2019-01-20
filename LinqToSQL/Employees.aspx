d2c8c3a524074516356df0711277ae18b13aeb73
Mandeep75 <mandeep75.singh@gmail.com>
2019-01-20 20:38:06 +1100

    Initial checkin

---
+++ LinqToSQL/Employees.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Employees.aspx.cs" Inherits="LinqToSQL.Employees" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+            <br />
+            <asp:Button ID="btnGetData" runat="server" BackColor="#FF3300" OnClick="btnGetData_Click" Text="GetData" />
+&nbsp;<asp:Button ID="btnInsert" runat="server" OnClick="btnInsert_Click" Text="Insert" />
+&nbsp;<asp:Button ID="btnUpdate" runat="server" OnClick="btnUpdate_Click" Text="Update" />
+&nbsp;<asp:Button ID="btnDelete" runat="server" OnClick="btnDelete_Click" Text="Delete" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ LinqToSQL/Employees.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LinqToSQL
+{
+    public partial class Employees : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           // GetData();
+
+        }
+
+        private void GetData()
+        {
+            var dataContext = new SampleDataContext();
+            //var maleEmployees = from employee in dataContext.DepartmentEmployees
+            //                    where employee.Gender == "Male"
+            //                    orderby employee.EmployeeId descending
+            //                    select employee;
+
+           // var maleEmployees = dataContext.DepartmentEmployees.Where(emp => emp.Gender == "Male").OrderBy(emp => emp.EmployeeId);
+            GridView1.DataSource = dataContext.DepartmentEmployees;
+            GridView1.DataBind();
+        }
+
+        protected void btnGetData_Click(object sender, EventArgs e)
+        {
+            GetData();
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            using (var dbContext=new SampleDataContext())
+            {
+                var employee = new DepartmentEmployee()
+                {
+                    EmployeeId = 100,
+                    Name = "Sikandar Sultan",
+                    Gender = "Male",
+                    City = "Patna",
+                    DepartmentId = 3
+                };
+                dbContext.DepartmentEmployees.InsertOnSubmit(employee);
+                dbContext.SubmitChanges();
+
+            }
+            GetData();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            using (var dbContext = new SampleDataContext())
+            {
+                var employee = (from Deptemployee in dbContext.DepartmentEmployees
+                                where Deptemployee.Name == "Sikandar Sultan" select Deptemployee).SingleOrDefault();
+
+                employee.Name = "Sikandaar Mahaan";
+                
+                dbContext.SubmitChanges();
+
+            }
+            GetData();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (var dbContext = new SampleDataContext())
+            {
+                var employee = (from Deptemployee in dbContext.DepartmentEmployees
+                                where Deptemployee.Name == "Sikandaar Mahaan"
+                                select Deptemployee).SingleOrDefault();
+
+                dbContext.DepartmentEmployees.DeleteOnSubmit(employee);
+
+                dbContext.SubmitChanges();
+
+            }
+            GetData();
+        }
+    }
+}
